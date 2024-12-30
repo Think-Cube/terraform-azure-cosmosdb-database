f1fc44f7002a6e358a1b5cd12148abc4dbd68b04
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2024-12-30 13:25:37 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,35 +1,26 @@
-############################
-# Common vars
-############################
 variable "environment" {
-  description = "Variable used for backend container name key."
+  description = "Var used for backend container name key"
   type        = string
   default     = "dev"
 }
 
-############################
-# Resource group vars
-############################
 variable "resource_group_location" {
-  description = "Specifies the supported Azure location where the resource group exists. Changing this forces a new resource to be created."
+  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
   default     = "West Europe"
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create resources. Changing this forces a new resource to be created."
+  description = "The name of the resource group in which to create the Redis instance. Changing this forces a new resource to be created."
   type        = string
 }
 
-############################
-# CosmosDB Database variables
-############################
 variable "cosmosdb_account_name" {
-  description = "Specifies the name of the Cosmos DB account. Changing this forces a new resource to be created."
+  description = "Specifies the name of the Cosmos DB SQL Container. Changing this forces a new resource to be created."
   type        = string
 }
 
 variable "cosmosdb_sql_database_name" {
-  description = "Specifies the name of the Cosmos DB SQL database. Changing this forces a new resource to be created."
+  description = "Specifies the name of the Cosmos DB SQL Database. Changing this forces a new resource to be created."
   type        = string
 }
